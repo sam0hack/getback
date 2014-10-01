91e501bf7cfacfbd712699a1c49ac7e70ad8d99a
codex8 <sam.nyx@live.com>
2014-10-01 17:30:28 +0530

    Version2

---
+++ GetBack/init.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Getback version 2
+Copyright (C) 2014 Ilm tech lab , (http://ilmtechlab.com/).
+License GNU GENERAL PUBLIC LICENSE  <http://fsf.org/>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+Get Update from GITHUB (https://github.com/codex8/getback/).
+*/
+require 'core/Loader.php';
+
+$load = new Loader();
+
+$load->logo(); #Load Logo
+
+
+$load->main(); #load main program
+
+
+?>
